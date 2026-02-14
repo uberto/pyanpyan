4b4cedbfd4651bf10527541aca10515a30a0c5bb
Uberto Barbini <uberto.barbini@kraken.tech>
2026-02-14 21:27:03 +0900

    feat(serialization): add @Serializable to ChecklistItemState sealed interface

--- common/src/commonMain/kotlin/com/pyanpyan/domain/model/ChecklistItemState.kt
+++ common/src/commonMain/kotlin/com/pyanpyan/domain/model/ChecklistItemState.kt
@@ -1,7 +1,19 @@
 package com.pyanpyan.domain.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface ChecklistItemState {
+    @Serializable
+    @SerialName("Pending")
     data object Pending : ChecklistItemState
+
+    @Serializable
+    @SerialName("Done")
     data object Done : ChecklistItemState
+
+    @Serializable
+    @SerialName("IgnoredToday")
     data object IgnoredToday : ChecklistItemState
 }
