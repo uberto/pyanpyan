0ea88dc6d049a56856aa7b7f19a2720d6b52d73b
Uberto Barbini <uberto.barbini@kraken.tech>
2026-02-14 21:32:23 +0900

    feat(serialization): add @Serializable to ChecklistSchedule

--- common/src/commonMain/kotlin/com/pyanpyan/domain/model/ChecklistSchedule.kt
+++ common/src/commonMain/kotlin/com/pyanpyan/domain/model/ChecklistSchedule.kt
@@ -1,7 +1,9 @@
 package com.pyanpyan.domain.model
 
 import kotlinx.datetime.DayOfWeek
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ChecklistSchedule(
     val daysOfWeek: Set<DayOfWeek>, // empty set = all days
     val timeRange: TimeRange
